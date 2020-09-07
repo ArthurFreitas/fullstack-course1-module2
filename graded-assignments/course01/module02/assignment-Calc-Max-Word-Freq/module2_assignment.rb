e99b5da7ad3fc0d7909821bf2bf6a704d8e877d4
Arthur Freitas <bfreitas.arthur@gmail.com>
2020-09-07 21:03:50 +0000

    Add module 2 graded assignment solution

--- graded-assignments/course01/module02/assignment-Calc-Max-Word-Freq/module2_assignment.rb
+++ graded-assignments/course01/module02/assignment-Calc-Max-Word-Freq/module2_assignment.rb
@@ -8,6 +8,8 @@ class LineAnalyzer
   #* content          - the string analyzed (provided)
   #* line_number      - the line number analyzed (provided)
 
+  attr_reader :highest_wf_count, :highest_wf_words, :content, :line_number
+
   #Add the following methods in the LineAnalyzer class.
   #* initialize() - taking a line of text (content) and a line number
   #* calculate_word_frequency() - calculates result
@@ -16,22 +18,40 @@ class LineAnalyzer
   #* take in a line of text and line number
   #* initialize the content and line_number attributes
   #* call the calculate_word_frequency() method.
+  def initialize (content, line_number)
+    @content = content
+    @line_number = line_number
+    calculate_word_frequency
+  end
 
   #Implement the calculate_word_frequency() method to:
   #* calculate the maximum number of times a single word appears within
   #  provided content and store that in the highest_wf_count attribute.
   #* identify the words that were used the maximum number of times and
   #  store that in the highest_wf_words attribute.
+  def calculate_word_frequency()
+    word_frequency = Hash.new(0)
+    @content.split.each do |word|
+      word_frequency[word.downcase] += 1
+    end
+    @highest_wf_count = word_frequency.values.max 
+    @highest_wf_words = word_frequency.select{|k,v| v == @highest_wf_count}.map{|k,v| k}
+  end
 end
 
 #  Implement a class called Solution. 
 class Solution
 
-  # Implement the following read-only attributes in the Solution class.
+  # Implement the following read-only attributes in the Solution class.calculate_word_frequency
   #* analyzers - an array of LineAnalyzer objects for each line in the file
   #* highest_count_across_lines - a number with the maximum value for highest_wf_words attribute in the analyzers array.
   #* highest_count_words_across_lines - a filtered array of LineAnalyzer objects with the highest_wf_words attribute 
   #  equal to the highest_count_across_lines determined previously.
+  attr_reader :analyzers, :highest_count_across_lines, :highest_count_words_across_lines
+
+  def initialize()
+    @analyzers = []
+  end
 
   # Implement the following methods in the Solution class.
   #* analyze_file() - processes 'test.txt' intro an array of LineAnalyzers and stores them in analyzers.
@@ -43,13 +63,28 @@ class Solution
   # Implement the analyze_file() method() to:
   #* Read the 'test.txt' file in lines 
   #* Create an array of LineAnalyzers for each line in the file
+  def analyze_file()
+    File.open('test.txt').each_with_index do |line, index|
+      @analyzers.push(LineAnalyzer.new(line, index))
+    end
+  end
 
   # Implement the calculate_line_with_highest_frequency() method to:
   #* calculate the maximum value for highest_wf_count contained by the LineAnalyzer objects in analyzers array
   #  and stores this result in the highest_count_across_lines attribute.
   #* identifies the LineAnalyzer objects in the analyzers array that have highest_wf_count equal to highest_count_across_lines 
   #  attribute value determined previously and stores them in highest_count_words_across_lines.
+  def calculate_line_with_highest_frequency()
+    @highest_count_across_lines = @analyzers.max_by{|a| a.highest_wf_count}.highest_wf_count
+    @highest_count_words_across_lines = @analyzers.select{|a| a.highest_wf_count == @highest_count_across_lines}
+  end
 
   #Implement the print_highest_word_frequency_across_lines() method to
   #* print the values of objects in highest_count_words_across_lines in the specified format
+  def print_highest_word_frequency_across_lines()
+    puts "The following words have the highest word frequency per line:"
+    @analyzers.each do |analyzer|
+      puts "#{analyzer.highest_wf_words} appears in line #{analyzer.line_number + 1}"
+    end 
+  end
 end
